62010ce4ad71ebe5a3eb60ca97af7331123a803a
norio c <norionorioc@gmail.com>
2020-05-18 20:48:50 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -13,10 +13,11 @@ class Game
     [6,4,2]
   ]
   
-  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
-    @board=board
-    @player_1=player_1
-    @player_2=player_2
+  def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+    @board.display
   end
   
   def current_player
