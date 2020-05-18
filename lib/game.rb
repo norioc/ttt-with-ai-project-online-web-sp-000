3af431da73dd9d27bfd1fb8a54365880c0c73f40
norio c <norionorioc@gmail.com>
2020-05-18 20:42:10 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -25,10 +25,13 @@ class Game
   
   def won? 
     WIN_COMBINATIONS.detect do |combo|
-      @board.cells[combo[0]] == @board.cells[combo[1]] &&
+      if @board.cells[combo[0]] == @board.cells[combo[1]] &&
       @board.cells[combo[1]] == @board.cells[combo[2]] &&
       @board.taken?(combo[0]+1)
-    end
+     return combination
+        end
+      end
+    return false
   end
   
   def draw?
